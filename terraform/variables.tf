c925f9b0ca13aed04f6a304f9a1bcc02fa98cb93
kashireddy <kashireddynaani9967@gmail.com>
2023-10-19 17:54:02 +0530

    nexus url changed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,5 +17,5 @@ variable "common_tags" {
 
 variable "app_version" {
   # this is jsut to test variable is going from terraform to shell and then to ansible
-  default = "1.10.0"
+  default = "100.00.00"
 }
